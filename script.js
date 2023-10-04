5dbc65514fe9f8628b86c8a046a9e2fefe55400c
IMTheBale <johnny.bale.play@gmail.com>
2023-10-04 14:07:03 +0300

    created script

---
+++ script.js
@@ -0,0 +1,6 @@
+const account = "regular";
+let accountStatus = "active";
+const isAdmin = account == "admin";
+const isValid = accountStatus !== "inactive";
+console.log("Admin: " + isAdmin);
+console.log("Valid: " + isValid);
